4bd9d7a4abd44f106e51f2ccdc4d5c7f04b4b3cd
bashir-ahmed <babashir8811@gmail.com>
2021-04-06 09:39:11 +0600

    Demo
    Demo

---
+++ app.js
@@ -0,0 +1,81 @@
+// owl carousel for hero section
+$(".owl-carousel").owlCarousel({
+	loop: true,
+	margin: 10,
+	autoplay: true,
+	rtl: true,
+	autoplayTimeout: 4000,
+	autoplayHoverPause: true,
+	nav: false,
+	dots: false,
+	responsive: {
+		0: {
+			items: 1,
+		},
+		600: {
+			items: 1,
+		},
+		1000: {
+			items: 1,
+		},
+	},
+});
+
+// slick slider
+$(".slider").slick({
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 1000,
+	dots: false,
+	infinite: true,
+	infinite: true,
+	speed: 800,
+	centerMode: true,
+	centerPadding: "60px",
+	responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				arrows: true,
+				centerMode: true,
+				centerPadding: "40px",
+				slidesToShow: 2,
+			},
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				arrows: true,
+				centerMode: true,
+				centerPadding: "40px",
+				slidesToShow: 1,
+			},
+		},
+	],
+});
+// hamburger menue
+const LinkSection = document.querySelector(".linkSec");
+const ham = document.querySelector(".bars");
+ham.addEventListener("click", function () {
+	LinkSection.classList.toggle("linkSecShow");
+});
+
+// changing navbar bg color
+window.addEventListener("scroll", () => {
+	const nav = document.querySelector("nav");
+	if (window.scrollY > 600) {
+		nav.classList.add("scroll");
+	} else {
+		nav.classList.remove("scroll");
+	}
+});
+// up Arrow
+window.addEventListener("scroll", () => {
+	const arrow = document.querySelector(".upArrow");
+	if (window.scrollY > 950) {
+		arrow.classList.add("arrow");
+	} else {
+		arrow.classList.remove("arrow");
+	}
+});
